e53a5e0e324dff95a8201c940fe09f6a3236ea82
littlelittleJessica <446067382@qq.com>
2021-08-25 11:40:29 +0800

    2-7 增加hello接口测试 test.http

--- src/main/java/com/jess/wiki/controller/TestController.java
+++ src/main/java/com/jess/wiki/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.jess.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @Description
@@ -16,10 +19,15 @@ public class TestController {
     /**
      * 请求方式：GET POST PUT DELETE
      * RequestMapping表示接受所有方式的请求
-     * @return
+     * @return 返回Hello World!字符串
      */
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post" + name;
     }
 }
