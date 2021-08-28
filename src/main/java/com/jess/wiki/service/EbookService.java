aaf3c36d6386fad2f6c170f0ac6a1c590bfd4791
littlelittleJessica <446067382@qq.com>
2021-08-28 16:02:14 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/jess/wiki/service/EbookService.java
+++ src/main/java/com/jess/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.jess.wiki.domain.EbookExample;
 import com.jess.wiki.mapper.EbookMapper;
 import com.jess.wiki.req.EbookReq;
 import com.jess.wiki.resp.EbookResp;
+import com.jess.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -30,14 +31,16 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            //单个复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
 
-        return respList;
+        //工具类列表复制代替循环
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 
 }
