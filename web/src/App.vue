f3d711810fe28f2f0b26c7e35c484baee61abb4d
littlelittleJessica <446067382@qq.com>
2021-08-29 11:32:04 +0800

    5-4 加入Ant Design Vue list组件 集成图标组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-layout>
+  <a-layout id="components-layout-demo-top-side-2">
     <the-header></the-header>
     <router-view/>
     <the-footer></the-footer>
@@ -19,26 +19,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
