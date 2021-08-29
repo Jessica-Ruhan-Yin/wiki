1ce392bccdff3e97604f3ef8493825066d7d556c
littlelittleJessica <446067382@qq.com>
2021-08-29 13:10:38 +0800

    5-9 增加AOP，打印接口耗时，请求参数和返回参数 去掉过滤器、拦截器

--- src/main/java/com/jess/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/jess/wiki/config/SpringMvcConfig.java
@@ -1,25 +1,25 @@
-package com.jess.wiki.config;
-
-import com.jess.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-/**
- * @Description
- * @Author Jessica
- * @Version v
- * @Date 2021/8/29
- */
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.jess.wiki.config;
+//
+//import com.jess.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+///**
+// * @Description
+// * @Author Jessica
+// * @Version v
+// * @Date 2021/8/29
+// */
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
