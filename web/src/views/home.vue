177282e7ae23767fe2965c70a0896d66b32cbdbe
littlelittleJessica <446067382@qq.com>
2021-08-30 20:21:20 +0800

    7-8 点击二级分类时可以显示该分类下的电子书

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,8 +7,8 @@
           @click="handleClick"
       >
         <a-menu-item key="welcome">
-            <MailOutlined />
-            <span>欢迎</span>
+          <MailOutlined />
+          <span>欢迎</span>
         </a-menu-item>
         <a-sub-menu v-for="item in level1" :key="item.id">
           <template v-slot:title>
@@ -95,31 +95,37 @@ export default defineComponent({
     };
 
     const isShowWelcome = ref(true);
-    const handleClick = (value:any) => {
-      //console.log("menu click",value)
-    //   if(value.key==='welcome'){
-    //     isShowWelcome.value=true;
-    //   }else{
-    //     isShowWelcome.value=false;
-    //   }
-    // };
-      isShowWelcome.value = value.key === 'welcome';
-    };
+    let categoryId2 = 0;
 
-
-
-    onMounted(() => {
-      handleQueryCategory();
+    const handleQueryEbook = () => {
       axios.get("/ebook/list", {
         params: {
           page: 1,
-          size: 1000
+          size: 1000,
+          categoryId2: categoryId2
         }
       }).then((response) => {
         const data = response.data;
         ebooks.value = data.content.list;
         // ebooks1.books = data.content;
       });
+    };
+
+    const handleClick = (value: any) => {
+      // console.log("menu click", value)
+      if (value.key === 'welcome') {
+        isShowWelcome.value = true;
+      } else {
+        categoryId2 = value.key;
+        isShowWelcome.value = false;
+        handleQueryEbook();
+      }
+      // isShowWelcome.value = value.key === 'welcome';
+    };
+
+    onMounted(() => {
+      handleQueryCategory();
+      // handleQueryEbook();
     });
 
     return {
