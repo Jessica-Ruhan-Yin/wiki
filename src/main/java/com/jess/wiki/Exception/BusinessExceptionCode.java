d374b4541ef6438c1a3d9c7ff762e5dd335c08d4
littlelittleJessica <446067382@qq.com>
2021-09-02 20:31:14 +0800

    9-4 增加了校验用户名不能重复 增加自定义异常

---
+++ src/main/java/com/jess/wiki/Exception/BusinessExceptionCode.java
@@ -0,0 +1,28 @@
+package com.jess.wiki.Exception;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/9/2
+ */
+public enum BusinessExceptionCode {
+
+    USER_LOGIN_NAME_EXIST("登录名已存在"),
+    ;
+
+    private String desc;
+
+    BusinessExceptionCode(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
+
