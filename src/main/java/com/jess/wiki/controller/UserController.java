4caa5b67e96865d5dea8af8b605925e47491ff52
littlelittleJessica <446067382@qq.com>
2021-09-02 19:24:30 +0800

    9-3 自己复制的代码 有点问题

---
+++ src/main/java/com/jess/wiki/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.jess.wiki.controller;
+
+import com.jess.wiki.req.UserQueryReq;
+import com.jess.wiki.req.UserSaveReq;
+import com.jess.wiki.resp.CommonResp;
+import com.jess.wiki.resp.UserQueryResp;
+import com.jess.wiki.resp.PageResp;
+import com.jess.wiki.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/8/25
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid UserQueryReq req) {
+        CommonResp<PageResp<UserQueryResp>> resp = new CommonResp<>();
+        PageResp<UserQueryResp> list = userService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    //post请求，json方式的提交 需要增加@RequestBody注解才可以接收到
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        userService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        userService.delete(id);
+        return resp;
+    }
+}
---
+++ src/main/java/com/jess/wiki/req/UserQueryReq.java
@@ -0,0 +1,27 @@
+package com.jess.wiki.req;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/9/2
+ */
+public class UserQueryReq extends PageReq {
+
+    private String loginName;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserQueryReq{" +
+                "loginName='" + loginName + '\'' +
+                '}';
+    }
+}
