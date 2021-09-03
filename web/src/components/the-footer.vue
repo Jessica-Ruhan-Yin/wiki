3b41de452a8c8527337150e35a177311ad237f9a
littlelittleJessica <446067382@qq.com>
2021-09-03 19:33:43 +0800

    9-9 使用vuex保存用户信息，方便其它组件获取登录信息。遗留问题，刷新后值没有了

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,13 +1,21 @@
 <template>
   <a-layout-footer style="text-align: center">
-    Ssica电子书
+    Ssica电子书，欢迎{{user.name}}
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {computed, defineComponent} from 'vue';
+import store from "@/store";
 
 export default defineComponent({
-  name: 'the-footer'
+  name: 'the-footer',
+  setup(){
+    const user = computed(() => store.state.user);
+
+    return{
+      user
+    }
+  }
 });
 </script>
