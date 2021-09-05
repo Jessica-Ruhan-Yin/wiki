1796b557b388b862efc87a946703411286548a57
littlelittleJessica <446067382@qq.com>
2021-09-05 08:23:15 +0800

    未登录时隐藏管理菜单bug修复 将style改为v-if

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,7 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
+
     <a class="login-menu" v-show="!user.id" @click="showLoginModal">
       <span>登录</span>
     </a>
@@ -14,10 +15,10 @@
         <span>退出登录</span>
       </a>
     </a-popconfirm>
-
     <a class="login-menu" v-show="user.id">
       <span>您好，{{user.name}}</span>
     </a>
+
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -26,17 +27,17 @@
       <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="/about">
-        <router-link to="/about">关于我们</router-link>
-      </a-menu-item>
-      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
+      <a-menu-item key="/admin/user" v-if="user.id">
         <router-link to="/admin/user">用户管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook" :style="user.id? {} : {display:'none'}">
+      <a-menu-item key="/admin/ebook" v-if="user.id">
         <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/category" :style="user.id? {} : {display:'none'}">
+      <a-menu-item key="/admin/category" v-if="user.id">
         <router-link to="/admin/category">分类管理</router-link>
+      </a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">关于我们</router-link>
       </a-menu-item>
 
     </a-menu>
