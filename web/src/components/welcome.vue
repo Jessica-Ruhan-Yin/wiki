a1985e15bc2ac0a8d91200cdd55715f6a530e86f
littlelittleJessica <446067382@qq.com>
2021-09-05 21:26:16 +0800

    11-7 将欢迎文字提取成组件

---
+++ web/src/components/welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>111</h1>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+
+export default defineComponent({
+  name: 'welcome',
+  setup() {
+
+
+    return {}
+  }
+});
+</script>
+
