69fb8dc5747e2e2591de80778c79a5c3eff6d88d
littlelittleJessica <446067382@qq.com>
2021-09-05 23:28:08 +0800

    11-10 修改LOGO展示

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
+    <div class="logo">Ssica知识库</div>
 
     <a class="login-menu" v-show="!user.id" @click="showLoginModal">
       <span>登录</span>
@@ -133,10 +133,18 @@ export default defineComponent({
 </script>
 
 <style>
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
 .login-menu {
   float: right;
   color: white;
-  padding-left: 20px;
+  padding-left: 10px;
 }
 </style>
-
